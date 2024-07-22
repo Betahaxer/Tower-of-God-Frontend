e725a5a7a662c0c590d61921300ea4deb00ab96e
Betahaxer <yuwenpoh@gmail.com>
2024-07-22 22:45:52 +0800

    Added product details components for all categories, except monitor is not working as it does not have a category

---
+++ src/components/Specs.tsx
@@ -0,0 +1,95 @@
+import { List, ListItem, Text } from "@chakra-ui/react";
+import React from "react";
+
+interface Product {
+  category: string;
+  [key: string]: any;
+}
+
+interface ProductProps {
+  product: Product;
+  keys: { [key: string]: string[] };
+}
+
+const Specs: React.FC<ProductProps> = ({ product, keys }) => {
+  const placeholder = () => (
+    <List>
+      <ListItem>Details not available, sorry!</ListItem>
+    </List>
+  );
+  if (!keys[product.category]) {
+    return placeholder();
+  }
+  const keysToRender = keys[product.category];
+  function formatText(text: string): string {
+    let formattedText = text.replace(/_/g, " ");
+
+    formattedText = formattedText
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(" ");
+
+    return formattedText;
+  }
+
+  return (
+    <List spacing={2}>
+      {keysToRender.map((key, index) => {
+        if (typeof product[key] === "boolean") {
+          return (
+            <ListItem key={index}>
+              <Text as={"span"} fontWeight={"bold"}>
+                {formatText(key) + ":"}
+              </Text>{" "}
+              {product[key] ? "Yes" : "No"}
+            </ListItem>
+          );
+        }
+        if (product.category === "keyboard" && key === "size") {
+          return (
+            <ListItem key={index}>
+              <Text as={"span"} fontWeight={"bold"}>
+                {formatText(key) + ":"}
+              </Text>{" "}
+              {product[key] ? product[key] + "%" : "-"}
+            </ListItem>
+          );
+        }
+        if (
+          (product.category === "phone" && key === "size") ||
+          (product.category === "television" && key === "screen_size")
+        ) {
+          return (
+            <ListItem key={index}>
+              <Text as={"span"} fontWeight={"bold"}>
+                {formatText(key) + ":"}
+              </Text>{" "}
+              {product[key] ? product[key] + " in" : "-"}
+            </ListItem>
+          );
+        }
+        if (key === "battery_life") {
+          const result = product[key][0]?.split("(")[0].trim();
+          return (
+            <ListItem key={index}>
+              <Text as={"span"} fontWeight={"bold"}>
+                {formatText(key) + ":"}
+              </Text>{" "}
+              {result ? result : "-"}
+            </ListItem>
+          );
+        }
+        return (
+          <ListItem key={index}>
+            <Text as={"span"} fontWeight={"bold"}>
+              {formatText(key) + ":"}
+            </Text>{" "}
+            {product[key] ? product[key] : "-"}
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+};
+
+export default Specs;
