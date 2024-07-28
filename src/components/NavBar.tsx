2072e4e654391f41d6531877fdad9a65e597134a
Betahaxer <yuwenpoh@gmail.com>
2024-07-28 17:34:40 +0800

    Sign in button dark mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import {
   Box,
@@ -19,27 +19,27 @@ import {
   Image,
   HStack,
   useToast,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 import {
   HamburgerIcon,
   CloseIcon,
   ChevronDownIcon,
   ChevronRightIcon,
-} from "@chakra-ui/icons";
+} from '@chakra-ui/icons';
 
-import SearchBar from "./SearchBar";
-import { useLocation, useNavigate } from "react-router-dom";
-import axios from "axios";
-import { clearTokens, getTokens } from "../utils/storage";
-import { useAuth } from "../contexts/AuthContext";
-import { DarkModeButton } from "./DarkModeButton";
+import SearchBar from './SearchBar';
+import { useLocation, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { clearTokens, getTokens } from '../utils/storage';
+import { useAuth } from '../contexts/AuthContext';
+import { DarkModeButton } from './DarkModeButton';
 
 export default function NavBar() {
   const { isOpen, onToggle } = useDisclosure();
   const navigate = useNavigate();
   const location = useLocation();
   // dont show search bar in nav bar in homepage
-  const showSearchBar = location.pathname === "/" ? false : true;
+  const showSearchBar = location.pathname === '/' ? false : true;
   const { isLoggedIn, login, logout } = useAuth();
   const toast = useToast();
   const handleLogout = async () => {
@@ -48,45 +48,45 @@ export default function NavBar() {
 
     try {
       const response = await axios.post(
-        "/api/logout/",
+        '/api/logout/',
         {}, // Empty body for the POST request
         {
           headers: {
             ...(accessToken && { Authorization: `Bearer ${accessToken}` }),
-            ...(refreshToken && { "Refresh-Token": refreshToken }),
+            ...(refreshToken && { 'Refresh-Token': refreshToken }),
           },
-        }
+        },
       );
       logout();
-      console.log("Response:", response);
+      console.log('Response:', response);
     } catch (error) {
       logout();
-      console.error("Error:", error);
+      console.error('Error:', error);
     }
   };
 
   return (
     <Box>
       <Flex
-        bg={useColorModeValue("white", "gray.800")}
-        color={useColorModeValue("gray.600", "white")}
-        minH={"70px"}
+        bg={useColorModeValue('white', 'gray.800')}
+        color={useColorModeValue('gray.600', 'white')}
+        minH={'70px'}
         py={{ base: 2 }}
         px={{ base: 4 }}
         borderBottom={1}
-        borderStyle={"solid"}
-        borderColor={useColorModeValue("gray.200", "gray.900")}
-        align={"center"}
+        borderStyle={'solid'}
+        borderColor={useColorModeValue('gray.200', 'gray.900')}
+        align={'center'}
       >
         <Flex
           flex={{ base: 1 }}
-          justify={{ base: "center", md: "start" }}
+          justify={{ base: 'center', md: 'start' }}
           align="center"
           gap="10"
           px="5"
         >
           <HStack
-            onClick={() => navigate("/")}
+            onClick={() => navigate('/')}
             justifyContent="center"
             alignItems="center"
           >
@@ -97,19 +97,19 @@ export default function NavBar() {
               objectFit="contain"
             />
             <Text
-              textAlign={useBreakpointValue({ base: "center", md: "left" })}
-              fontFamily={"heading"}
-              color={useColorModeValue("green.400", "white")}
+              textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
+              fontFamily={'heading'}
+              color={useColorModeValue('green.400', 'white')}
               fontWeight={800}
               mb={0}
               fontSize="xl"
-              _hover={{ cursor: "pointer" }}
+              _hover={{ cursor: 'pointer' }}
             >
               Choice
             </Text>
           </HStack>
 
-          <Flex display={{ base: "none", md: "flex" }} ml={0}>
+          <Flex display={{ base: 'none', md: 'flex' }} ml={0}>
             <DesktopNav />
           </Flex>
           {showSearchBar && <SearchBar></SearchBar>}
@@ -117,37 +117,36 @@ export default function NavBar() {
 
         <Stack
           flex={{ base: 1, md: 0 }}
-          justify={"flex-end"}
-          direction={"row"}
+          justify={'flex-end'}
+          direction={'row'}
           spacing={3}
         >
           <DarkModeButton />
           {!isLoggedIn && (
             <>
               <Button
-                fontSize={"sm"}
+                fontSize={'sm'}
                 fontWeight={500}
-                bg="white"
-                color="gray.600"
-                onClick={() => navigate("/login")}
+                bg={useColorModeValue('white', 'gray.600')}
+                color={useColorModeValue('gray.600', 'white')}
+                onClick={() => navigate('/login')}
                 _hover={{
-                  textDecoration: "none",
-                  color: "gray.800",
+                  textDecoration: 'none',
                 }}
               >
                 Sign In
               </Button>
               <Button
-                display={{ base: "none", md: "inline-flex" }}
-                fontSize={"sm"}
+                display={{ base: 'none', md: 'inline-flex' }}
+                fontSize={'sm'}
                 fontWeight={500}
-                color={"white"}
-                bg={"green.400"}
+                color={'white'}
+                bg={'green.400'}
                 _hover={{
-                  textDecoration: "none",
-                  bg: "green.500",
+                  textDecoration: 'none',
+                  bg: 'green.500',
                 }}
-                onClick={() => navigate("/register")}
+                onClick={() => navigate('/register')}
               >
                 Sign Up
               </Button>
@@ -155,21 +154,21 @@ export default function NavBar() {
           )}
           {isLoggedIn && (
             <Button
-              display={{ base: "none", md: "inline-flex" }}
-              fontSize={"sm"}
+              display={{ base: 'none', md: 'inline-flex' }}
+              fontSize={'sm'}
               fontWeight={500}
-              color={"white"}
-              bg={"green.400"}
+              color={'white'}
+              bg={'green.400'}
               _hover={{
-                textDecoration: "none",
-                bg: "green.500",
+                textDecoration: 'none',
+                bg: 'green.500',
               }}
               onClick={() => {
                 handleLogout();
-                navigate("/");
+                navigate('/');
                 toast({
-                  title: "Logged out!",
-                  status: "success",
+                  title: 'Logged out!',
+                  status: 'success',
                   duration: 2000,
                   isClosable: true,
                 });
@@ -185,25 +184,25 @@ export default function NavBar() {
 }
 
 const DesktopNav = () => {
-  const linkColor = useColorModeValue("gray.600", "gray.200");
-  const linkHoverColor = useColorModeValue("gray.800", "white");
-  const popoverContentBgColor = useColorModeValue("white", "gray.800");
+  const linkColor = useColorModeValue('gray.600', 'gray.200');
+  const linkHoverColor = useColorModeValue('gray.800', 'white');
+  const popoverContentBgColor = useColorModeValue('white', 'gray.800');
 
   return (
-    <Stack direction={"row"} spacing={4}>
+    <Stack direction={'row'} spacing={4}>
       {NAV_ITEMS.map((navItem) => (
         <Box key={navItem.label}>
-          <Popover trigger={"hover"} placement={"bottom-start"}>
+          <Popover trigger={'hover'} placement={'bottom-start'}>
             <PopoverTrigger>
               <Box
                 as="a"
                 p={2}
-                href={navItem.href ?? "#"}
-                fontSize={"sm"}
+                href={navItem.href ?? '#'}
+                fontSize={'sm'}
                 fontWeight={500}
                 color={linkColor}
                 _hover={{
-                  textDecoration: "none",
+                  textDecoration: 'none',
                   color: linkHoverColor,
                 }}
               >
@@ -214,17 +213,17 @@ const DesktopNav = () => {
             {navItem.children && (
               <PopoverContent
                 border={0}
-                boxShadow={"xl"}
+                boxShadow={'xl'}
                 bg={popoverContentBgColor}
                 p={4}
-                rounded={"xl"}
+                rounded={'xl'}
                 maxW="70%"
                 maxH="80vh"
               >
                 <Stack
                   overflow="scroll"
                   css={{
-                    cursor: "pointer",
+                    cursor: 'pointer',
                   }}
                 >
                   {navItem.children.map((child) => (
@@ -244,37 +243,37 @@ const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
   const navigate = useNavigate();
   const onSearch = async (query: string) => {
     try {
-      let url = `/api/products/`;
+      const url = `/api/products/`;
       // querying the database based on category and simple filtering with user query
       const response = await axios.get(url, {
         params: { q: query },
       });
 
       // passing the data to the search results page
-      navigate("/search", { state: { query } });
+      navigate('/search', { state: { query } });
     } catch (error) {
-      console.error("Error fetching search results:", error);
+      console.error('Error fetching search results:', error);
     }
   };
   return (
     <Box
       as="a"
       href={href}
-      role={"group"}
-      display={"block"}
+      role={'group'}
+      display={'block'}
       p={2}
-      rounded={"md"}
-      _hover={{ bg: useColorModeValue("green.50", "gray.900") }}
+      rounded={'md'}
+      _hover={{ bg: useColorModeValue('green.50', 'gray.900') }}
       onClick={() => {
         onSearch(label);
       }}
     >
-      <Stack direction={"row"} align={"center"}>
+      <Stack direction={'row'} align={'center'}>
         <Box>
           <Text
             m="2"
-            transition={"all .3s ease"}
-            _groupHover={{ color: "green.400" }}
+            transition={'all .3s ease'}
+            _groupHover={{ color: 'green.400' }}
             fontWeight={500}
             textTransform="capitalize"
           >
@@ -282,15 +281,15 @@ const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
           </Text>
         </Box>
         <Flex
-          transition={"all .3s ease"}
-          transform={"translateX(-10px)"}
+          transition={'all .3s ease'}
+          transform={'translateX(-10px)'}
           opacity={0}
-          _groupHover={{ opacity: "100%", transform: "translateX(0)" }}
-          justify={"flex-end"}
-          align={"center"}
+          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+          justify={'flex-end'}
+          align={'center'}
           flex={1}
         >
-          <Icon color={"green.400"} w={5} h={5} as={ChevronRightIcon} />
+          <Icon color={'green.400'} w={5} h={5} as={ChevronRightIcon} />
         </Flex>
       </Stack>
     </Box>
@@ -306,52 +305,52 @@ interface NavItem {
 
 const NAV_ITEMS: Array<NavItem> = [
   {
-    label: "About",
-    href: "/about",
+    label: 'About',
+    href: '/about',
   },
   {
-    label: "Categories",
+    label: 'Categories',
     children: [
       {
-        label: "earphones",
-        subLabel: "Explore more...",
+        label: 'earphones',
+        subLabel: 'Explore more...',
       },
       {
-        label: "keyboard",
-        subLabel: "Explore more",
+        label: 'keyboard',
+        subLabel: 'Explore more',
       },
       {
-        label: "laptop",
-        subLabel: "Explore more",
+        label: 'laptop',
+        subLabel: 'Explore more',
       },
       {
-        label: "mouse",
-        subLabel: "Explore more",
+        label: 'mouse',
+        subLabel: 'Explore more',
       },
       {
-        label: "phone",
-        subLabel: "Explore more",
+        label: 'phone',
+        subLabel: 'Explore more',
       },
       {
-        label: "monitor",
-        subLabel: "Explore more",
+        label: 'monitor',
+        subLabel: 'Explore more',
       },
       {
-        label: "speaker",
-        subLabel: "Explore more",
+        label: 'speaker',
+        subLabel: 'Explore more',
       },
       {
-        label: "television",
-        subLabel: "Explore more",
+        label: 'television',
+        subLabel: 'Explore more',
       },
     ],
   },
   {
-    label: "Compare",
-    href: "/compare",
+    label: 'Compare',
+    href: '/compare',
   },
   {
-    label: "Wishlist",
-    href: "/wishlist",
+    label: 'Wishlist',
+    href: '/wishlist',
   },
 ];
